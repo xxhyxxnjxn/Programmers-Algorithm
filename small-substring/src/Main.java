1d3dc25fbf98ca9d222a34f9b30905d815a8d75f
xxhyxxnjxn <wjdguswls55@gmail.com>
2024-12-16 19:59:54 +0900

    알고리즘 문제 Repository 초기화

--- small-substring/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int answer = solution.solution("10203", "15");
-        System.out.println("answer : "+answer);
-    }
-}
---
+++ src/test.java
@@ -0,0 +1,7 @@
+package src;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
